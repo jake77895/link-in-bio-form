d51f8733746f69e28873bddd5483faf49f1dde2c
jake77895 <jspitzer@chicagobooth.edu>
2024-11-07 20:42:28 +0000

    Commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,11 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def input
+    @list_of_items = Item.all
+
+    render({ :template => "item_templates/input" })
+  end
+
 end
